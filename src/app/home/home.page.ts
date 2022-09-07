66cdfc9df99ad7eca0d5de2033b7ca31b06e6eae
Aimee Rodríguez <arodriguez.posma@gmail.com>
2022-09-07 18:08:54 -0400

    Agregados slides

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,13 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  slideOpts = {
+    initialSlide: 0, // Indica que el slide inicial será el primero
+    slidesPerView: 1, // Indica que solo se permita un slide por vista
+    centeredSlides: true, // Indica que los slides estarán centrados
+    speed: 400 // Indica que la velocidad de transición seará de 400 ms
+  }
+
   constructor() {}
 
 }
