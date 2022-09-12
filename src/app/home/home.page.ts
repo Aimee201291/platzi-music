dbecaf159117e56678cd1192215302a7ec751f13
Aimee Rodríguez <arodriguez.posma@gmail.com>
2022-09-12 11:23:29 -0400

    Agregados slides dinámicos

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IonicSwiper } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -14,6 +15,33 @@ export class HomePage {
     speed: 400 // Indica que la velocidad de transición seará de 400 ms
   }
 
+  slides = [
+    {
+      imageSrc: "assets/img/logo.png",
+      imageAlt: "Platzi music logo",
+      title: "Escucha tu música",
+      subtitle: "En cualquier lugar",
+      description: "Los mejores álbumes, las mejores canciones. Escucha y comparte en cualquier momento, a todas horas.",
+      icon: "play"
+    },
+    {
+      imageSrc: "assets/img/logo.png",
+      imageAlt: "Platzi music logo",
+      title: "Disfruta de nuestro reproductor",
+      subtitle: "De videos increíbles",
+      description: "Entra al modo video de nuestro reproductor y obtén acceso a clips, documentales y making offs increíbles de tu artista favorito.",
+      icon: "videocam"
+    },
+    {
+      imageSrc: "assets/img/logo.png",
+      imageAlt: "Platzi music logo",
+      title: "Accede al exclusivo",
+      subtitle: "Modo deporte",
+      description: "Crea un playlist basado en tu actividad física. Ten reportes y acceso a lo que necesitas, intégralo con GPS.",
+      icon: "bicycle"
+    }
+  ]
+  
   constructor() {}
 
 }
