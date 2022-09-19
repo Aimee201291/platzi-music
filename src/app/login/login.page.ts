3cca3fe92d7009f91be5d158e085741395bf5e9c
Aimee Rodríguez <arodriguez.posma@gmail.com>
2022-09-18 22:04:52 -0400

    Ahora se muestran mensajes que le indican al usuario si los datos introducidos en el formulario de login no cumplen con los requisitos

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -9,15 +9,33 @@ import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms'
 export class LoginPage implements OnInit {
 
   loginForm: FormGroup;
+  validationMessages = { //Esta variable la definimos para mostrar en la pantalla los posibles mensajes de error
+    email: [            // según lo introducido por el usuario en el formulario
+      { type: "required", message: "El email es requerido" },
+      { type: "pattern", message: "El email introducido no es válido" }
+    ],
+    password: [
+      { type: "required", message: "El password es requerido" },
+      { type: "minlength", message: "El password debe contener al menos 5 letras" }
+    ]
+  }
 
   constructor(private formBuilder: FormBuilder) {
     this.loginForm = this.formBuilder.group({
-      email: new FormControl(
+      email: new FormControl( //El primer parámetro que recibe FormControl es qué valor va a tener por defecto el campo
         "", 
-        Validators.compose([
-        Validators.required,
-        Validators.pattern("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$")
-      ]))  //El primer parámetro que recibe FormControl es qué valor va a tener por defecto
+        Validators.compose([ 
+          Validators.required,
+          Validators.pattern("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$")
+        ])
+      ),
+      password: new FormControl(
+        "", 
+        Validators.compose([ 
+          Validators.required,
+          Validators.minLength(5)  // Estamos indicando que el password debe tener al menos 5 caracteres
+        ])
+      )   
     })
    }
 
